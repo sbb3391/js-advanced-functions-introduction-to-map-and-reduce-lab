6e88cdb74ce7f5131a00358db00a9608fc7048fd
sbb3391 <sbb33391@gmail.com>
2021-06-11 21:10:16 -0400

    commit tree 767fe281992066f2c499bac25a7a87a4ecd52eea

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+
+function mapToNegativize(items) {
+  return items.map( item => -item )
+}
+// mapToNoChange(sourceArray)
+
+function mapToNoChange(items) {
+  return items.map(item => item)
+}
+// mapToDouble(sourceArray)
+
+function mapToDouble(items) {
+  return items.map( item => item * 2)
+}
+// mapToSquare(sourceArray)
+
+function mapToSquare(items) {
+  return items.map( item => item * item)
+}
+
+
+
+// reduceToTotal(sourceArray, startingPoint)
+
+function reduceToTotal(items, memo = 0) {
+  return items.reduce((memo, item) => memo += item)
+}
+
+// reduceToAllTrue(sourceArray)
+
+function reduceToTrue(items) {
+  for (const item of items) {
+    if (!item) {
+      return false
+    }
+  }
+  return true
+}
+// reduceToAnyTrue(sourceArray)
+
+function reduceToAnyTrue (items) {
+  for (const item of items) {
+    if (item) {
+      return true
+    }
+  }
+  return false
+}
